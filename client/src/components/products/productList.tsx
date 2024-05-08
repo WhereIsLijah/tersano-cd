d711eef51f093c8af4b13acb29abcdbc9afa2fd3
WhereIsLijah <114761023+WhereIsLijah@users.noreply.github.com>
2024-05-08 01:01:58 -0400

    ProductList update

--- client/src/components/products/productList.tsx
+++ client/src/components/products/productList.tsx
@@ -1,12 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
-export default function ProductList(){
-    
-    return (
-        <>
-        </>
-    );
+interface Product{
+    _id: string;
+    name: string;
+    price: string;
+    description: string;
 }
 
 
+export default function ProductList(){
+    const [products, setProducts] = useState<Product[]>([]);
+    
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try{
+                const response = await fetch('http://localhost:4000/product/');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                setProducts(data);
+            } catch (error) {
+                console.error('Error Fetching Products!', error);
+            }
+        };
+        fetchProducts();
+    }, []);
+
+
+    return (
+        <div>
+            <h1>Products</h1>
+            {products.map(product => (
+                <div key={product._id}>
+                    <h2>{product.name}</h2>
+                    <p>Price: ${product.price}</p>
+                    <p>Description: {product.description}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
